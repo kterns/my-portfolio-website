20b34ef22882f10758ef340afd080030c64ff8bb
Kyle Terns <ternsk2@winthrop.edu>
2016-02-06 19:02:15 -0500

    Reduced background image size to increase performance

--- assets/scripts/parallax.js
+++ assets/scripts/parallax.js
@@ -1,11 +1,20 @@
+var pContainerHeight = $('section#about').height();
+
 $(window).scroll(function() {
-	var wScroll = $(window).scrollTop();
-	imgScroll(wScroll);
+	var wScroll = $(this).scrollTop();
+
+	if (wScroll <= pContainerHeight) {
+		imgScroll(wScroll);
+	}
 });
 
 function imgScroll(wScroll) {
 	var x = $('#about').css('background-position').split(" ")[0];
 	//console.log(x);
+	/* TODO: Use transform: translate instead? */
+	// $('#about').css({
+	// 	'transform' : 'translate(' + x + ', ' + (54 - wScroll) + 'px)'
+	// });
 	$('#about').css('background-position', x + ' ' + (54 - wScroll) + 'px');
 	//console.log(wScroll + 54);
 }
