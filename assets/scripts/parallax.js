4e219e5fd6f8940978c15e29301455d87b490821
Kyle Terns <ternsk2@winthrop.edu>
2016-02-09 18:28:08 -0500

    UI overhaul

--- assets/scripts/parallax.js
+++ assets/scripts/parallax.js
@@ -1,22 +1,22 @@
-var pContainerHeight = $('section#about').height();
-
-$(window).scroll(function() {
-	var wScroll = $(this).scrollTop();
-
-	if (wScroll <= pContainerHeight) {
-		imgScroll(wScroll);
-	}
-});
-
-function imgScroll(wScroll) {
-	var x = $('#about').css('background-position').split(" ")[0];
-	//console.log(x);
-	/* TODO: Use transform: translate instead? */
-	// $('#about').css({
-	// 	'transform' : 'translate(' + x + ', ' + (54 - wScroll) + 'px)'
-	// });
-	$('#about').css('background-position', x + ' ' + (54 - wScroll) + 'px');
-	//console.log(wScroll + 54);
-}
+// var pContainerHeight = $('section#about').height();
+//
+// $(window).scroll(function() {
+// 	var wScroll = $(this).scrollTop();
+//
+// 	if (wScroll <= pContainerHeight) {
+// 		imgScroll(wScroll);
+// 	}
+// });
+//
+// function imgScroll(wScroll) {
+// 	var x = $('#about').css('background-position').split(" ")[0];
+// 	//console.log(x);
+// 	/* TODO: Use transform: translate instead? */
+// 	// $('#about').css({
+// 	// 	'transform' : 'translate(' + x + ', ' + (54 - wScroll) + 'px)'
+// 	// });
+// 	$('#about').css('background-position', x + ' ' + (54 - wScroll) + 'px');
+// 	//console.log(wScroll + 54);
+// }
 
 /* TODO: Set x position on window resize and store globally */
